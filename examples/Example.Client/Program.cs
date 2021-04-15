80169ac07d935b19f2fd836d52dd9177bc2dfff6
Sly Gryphon <sly@gamertheory.net>
2021-04-15 22:43:07 +1000

    Runs and does nothing :-)

--- examples/Example.Client/Program.cs
+++ examples/Example.Client/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Example.Protocol;
@@ -13,11 +16,16 @@ using Microsoft.Extensions.Configuration;
 
 namespace Example.Client
 {
+    // dotnet run --project examples/Example.Client
     class Program
     {
+        private static List<IDisposable> _allListeners = new();
+
         static async Task Main(string[] args)
         {
             Console.WriteLine("CTRL+C to exit");
+            
+            SubscribeToAllLibp2pDiagnostics();
             
             var configuration = new ConfigurationBuilder()
                 .AddJsonFile("appsettings.json")
@@ -25,7 +33,8 @@ namespace Example.Client
                 .Build();
             
             var minimumDesired = configuration.GetValue<int>("Discovery:MinimumDesired");
-            var bootstrapAddresses = configuration.GetValue<string[]>("Discovery:Discovery");
+            var bootstrapAddresses = configuration.GetSection("Discovery:Discovery").GetChildren().Select(x => x.Value)
+                .ToArray();
             
             var transports = new ITransport[] {new TcpTransport()};
             var selectors = new IProtocolSelect[] {new MultistreamSelect1()};
@@ -39,16 +48,15 @@ namespace Example.Client
             var peerPool = new PeerPool();
             peerPool.ConfigureConnect(transports, selectors, encryptors, multiplexers);
             peerPool.ConfigureListen(transports, selectors, encryptors, multiplexers);
-            
             peerPool.AddDiscovery(discovery);
-            peerPool.MinimumDesired = configuration.GetValue<int>("Discovery:MinimumDesired");
+            peerPool.MinimumDesired = minimumDesired;
 
             var cts = new CancellationTokenSource();
             await peerPool.StartAsync(cts.Token);
 
             while (true)
             {
-                var connection = peerPool.AcceptConnectionAsync(cts.Token);
+                var connection = await peerPool.AcceptConnectionAsync(cts.Token);
                 if (connection.Direction == Direction.Outbound)
                 {
                     // A new outbound connection should initiate Status
@@ -65,7 +73,41 @@ namespace Example.Client
             // ALSO new connection => listen for response on stream
             // when status received (either on dialled or received), compare,
             // => if less, open new multiplex stream and send beacon block range request
-
         }
+        
+        private static void SubscribeToAllLibp2pDiagnostics()
+        {
+            _allListeners.Add(DiagnosticListener.AllListeners.Subscribe(listener =>
+            {
+                if (listener.Name.StartsWith("Libp2p."))
+                {
+                    _allListeners.Add(listener.Subscribe(kvp =>
+                    {
+                        if (kvp.Value is Activity activity)
+                        {
+                            if (kvp.Key.EndsWith(".Start"))
+                            {
+                                Console.WriteLine("{0}: {1} {2}-{3} {4}", kvp.Key,
+                                    activity.OperationName,
+                                    activity.TraceId, activity.SpanId, string.Concat(activity.Tags));
+                            }
+                            else
+                            {
+                                Console.WriteLine("{0}: {1}({2}) {3}-{4} {5}", kvp.Key,
+                                    activity.OperationName, activity.Duration,
+                                    activity.TraceId, activity.SpanId, string.Concat(activity.Tags));
+                            }
+                        }
+                        else
+                        {
+                            var currentActivity = Activity.Current;
+                            Console.WriteLine("{0}: {1} {2}-{3}", kvp.Key, kvp.Value, 
+                                currentActivity?.TraceId, currentActivity?.SpanId);
+                        }
+                    }));
+                }
+            }));
+        }
+
     }
 }
--- src/Libp2p.Peering/PeerPool.cs
+++ src/Libp2p.Peering/PeerPool.cs
@@ -10,34 +10,32 @@ namespace Libp2p.Peering
     {
         public void ConfigureConnect(IList<ITransport> transports, IList<IProtocolSelect> selectors, IList<IEncryptionProtocol> encryptors, IList<IMultiplexProtocol> multiplexers)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
         public void ConfigureListen(IList<ITransport> transports, IList<IProtocolSelect> selectors, IList<IEncryptionProtocol> encryptors, IList<IMultiplexProtocol> multiplexers)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
         public void AddDiscovery(IList<IDiscovery> discovery)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
 
         public int MinimumDesired { get; set; }
 
-        public void Start()
+        public async Task StartAsync(CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            await Task.Delay(0, cancellationToken);
+            //throw new NotImplementedException();
         }
 
-        public async Task StartAsync(CancellationToken ctsToken)
+        public async Task<IConnection> AcceptConnectionAsync(CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
-        }
-
-        public IConnection AcceptConnectionAsync(CancellationToken ctsToken)
-        {
-            throw new NotImplementedException();
+            await Task.Delay(5000, cancellationToken);
+            //throw new NotImplementedException();
+            return null;
         }
     }
 }
